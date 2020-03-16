040e6f2d22590b877babba83ae423ae078f534ae
Emery Green <emerygreen@emerys-mbp.home>
2020-03-16 18:47:34 -0500

    2 tests pass

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -14,4 +14,5 @@ class PagesController < ApplicationController
   def contact
   end
 
+ 
 end
--- spec/controllers/owner/products_controller_spec.rb
+++ spec/controllers/owner/products_controller_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Owner::ProductsController, type: :controller do
+  include Devise::Test::ControllerHelpers
 
+  describe "owner/products#new action" do
+    it "should redirect unauthenticated users to sign in page" do
+      get :new
+      expect(response.status).to eq(302)
+    end
+  end
 end
